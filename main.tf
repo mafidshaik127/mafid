ca9c045334411bbaf5f8f3e219f1b62209fb5226
mafidshaik127 <mafidshaik127@gmail.com>
2024-10-07 15:30:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,10 @@
-provider "aws" {
-region = "eu-central-1"
-}
-resource "aws_s3_bucket" "example" {
-  bucket = "my-tf-test-bucket"
+vim main.tf 
 
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
+provider "aws" {
+region = "us-east-1"
+}
+
+resource "aws_instance" "one" {
+ami = "ami-03eb6185d756497f8"
+instance_type = "t2.micro"
 }
